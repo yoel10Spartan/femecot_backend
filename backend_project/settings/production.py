f1dd09a279f9e1f85148d006ac753cffec5901a7
Yoel Muñoz Zecua <munozzecuayoel@gmail.com>
2022-04-29 13:18:10 -0500

    seccond commit

--- backend_project/settings/production.py
+++ backend_project/settings/production.py
@@ -6,7 +6,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'congreso',
+        'USER': 'root',
+        'PASSWORD': 'twJuL*2VHoq!c',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
