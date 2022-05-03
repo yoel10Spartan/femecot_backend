ada6702078aec9c315fa71ca58d9c1f5bfa131fd
Yoel Muñoz Zecua <munozzecuayoel@gmail.com>
2022-05-03 09:33:50 -0500

    Delete URL

--- backend_project/settings/base.py
+++ backend_project/settings/base.py
@@ -130,10 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 from decouple import config
 
-EMAIL_HOST_USER = 'munozzecuayoel@gmail.com'
-EMAIL_HOST_PASSWORD = 'spartan.123'
 EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_PORT = 587
+EMAIL_HOST_USER = 'munozzecuyoel.spartan1012@gmail.com'
+EMAIL_HOST_PASSWORD = 'spartan.123'
 EMAIL_USE_TLS = True
 
 # EMAIL_HOST = 'smtp.registroparacongresos.com'
